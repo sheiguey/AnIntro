dc4beedf75f956a798a3cff08c05ef1910114590
sheiguey <40198193+sheiguey@users.noreply.github.com>
2024-02-21 10:24:10 +0100

    navbar

--- src/app/navBar/nav.component.ts
+++ src/app/navBar/nav.component.ts
@@ -2,8 +2,7 @@ import { Component } from "@angular/core";
 
 @Component({
     selector:'app-navBar',
-    template:'<h1>NavBar components</h1>',
-   
+    templateUrl:'<h1>NavBar components</h1>',
 })
 
 export class NavBarComponent{
