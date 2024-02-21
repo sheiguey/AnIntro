b608cc4f2cfe7d71376247efc1559760ab0573f1
sheiguey <40198193+sheiguey@users.noreply.github.com>
2024-02-21 09:40:26 +0100

    creation navbar for test component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavBarComponent } from './navBar/nav.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,NavBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/navBar/nav.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector:'app-navBar',
+    template:'<h1>NavBar components</h1>',
+    styles:''
+})
+
+export class NavBarComponent{
+
+}
