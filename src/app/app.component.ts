6dd955fe4b85354a41a0b9caac6c383ef210dffe
sheiguey <40198193+sheiguey@users.noreply.github.com>
2024-02-21 10:11:33 +0100

    uiwjw

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NavBarComponent } from './navBar/nav.component';
+
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,NavBarComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
