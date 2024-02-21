662773f052d163dd96a2c9dfbd75563b13c44ab7
sheiguey <40198193+sheiguey@users.noreply.github.com>
2024-02-21 10:38:29 +0100

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { NavBarComponent } from './navBar/nav.component';
-
-
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet,NavBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'AnIntro';
+  title = 'my-app';
 }
